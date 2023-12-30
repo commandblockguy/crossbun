5a2dfe1fdb89d7339687ec78cc6f1bb89f3cad31
John Cesarz <bluestemjc@gmail.com>
2023-12-29 20:29:46 -0600

    Create puzzle converter

--- convert.py
+++ convert.py
@@ -1,15 +1,89 @@
 import puz
-import json
+import struct
+import subprocess
+import bitstring
+import argparse
+import pathlib
+from tivars.types import TIAppVar
 
-p = puz.load(open('test.puz', 'rb').read())
+parser = argparse.ArgumentParser(
+                    prog='convert.py',
+                    description='Converts .puz files into .8xv puzzle packs')
+parser.add_argument('puzzles', metavar='file', type=str, nargs='+',
+                    help='a .puz file')
+parser.add_argument('output', type=str, help='output appvar file')
+parser.add_argument('-t', '--title', required=True, help='pack title')
+parser.add_argument('-d', '--description', required=True, help='pack description')
+parser.add_argument('-n', '--varname', help='appvar name')
 
-print(p.solution)
-print(p.width, p.height)
-print(p.clues)
+args = parser.parse_args()
 
-print(f'''    const char *clues[] = {{
-{
-''.join(f'        {json.dumps(clue)},{chr(10)}' for clue in p.clues)}
-    }};
-    struct puzzle *puzzle = puzzle_new({p.width}, {p.height}, "{p.solution}", clues);
-''')
+def zx0_compress(b):
+    cmd = ['convbin', '-i', '/dev/stdin', '-o', '/dev/stdout', '-j', 'bin', '-k', 'bin', '-l', '0', '-c', 'zx0']
+    p = subprocess.run(cmd, capture_output=True, input=b)
+    if p.returncode != 0: raise RuntimeError('convbin failed')
+    return p.stdout
+
+def markup_to_bitmap(m):
+    return bitstring.Bits([m == 0x80 for c in m]).tobytes()
+
+magic = b'CROSS'
+version = 0
+
+header_format = '<5sBHHB'
+entry_format = '<HHHHBBHBHHH'
+
+def read_puzzle(filename):
+    with open(filename, 'rb') as f:
+        return puz.load(f.read())
+
+puzzles = [read_puzzle(f) for f in args.puzzles]
+
+pool_start = struct.calcsize(header_format) + struct.calcsize(entry_format) * len(puzzles)
+pool = b''
+pool_locs = {}
+
+def pool_add(s):
+    global pool
+    offset = len(pool)
+    b = s.encode('ascii') + b'\0' if isinstance(s, str) else s
+    if b in pool_locs:
+        return pool_locs[b]
+    else:
+        pool += b
+        pool_locs[b] = pool_start + offset
+        return pool_start + offset
+
+
+header = struct.pack(header_format, magic, version, pool_add(args.title), pool_add(args.description), len(puzzles))
+
+entries = [
+    struct.pack(
+        entry_format,
+        pool_add(p.title),
+        pool_add(p.notes),
+        pool_add(p.author),
+        pool_add(p.copyright),
+        p.width,
+        p.height,
+        pool_add(zx0_compress(p.solution.encode('ascii'))),
+        len(p.clues),
+        len(clue_data),
+        pool_add(zx0_compress(clue_data)),
+        pool_add(zx0_compress(markup_to_bitmap(p.markup().markup))) if p.has_markup() else 0,
+    ) for p in puzzles
+        for clue_data in [bytes(b for c in p.clues for b in c.encode('ascii') + b'\0')]
+]
+
+if args.varname is None:
+    varname = pathlib.Path(args.output).stem.upper()[:8]
+else:
+    varname = args.varname
+
+assert len(header + b''.join(entries)) == pool_start
+
+data = header + b''.join(entries) + pool
+
+TIAppVar(name=varname, archived=True, data=data).save(args.output)
+
+print(f'wrote {len(puzzles)} puzzles (total {len(data)} bytes) to appvar {varname} in {args.output}')
